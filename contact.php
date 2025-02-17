504fc02e948f774840af8f2566a877152f22e3d5
Pravalli1 <pentakotapravallika6@gmail.com>
2025-02-17 10:41:39 +0530

    latest update

--- contact.php
+++ contact.php
@@ -10,7 +10,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $body = "Name: $name\nEmail: $email\nNumber: $number\nMessage: $message";
 
     // Send email to both recipients
-    mail("naresh.narnapati@reymould.com", $subject, $body);
+    mail("pravallika.pentakota@reymould.com", $subject, $body);
 
     if (mail($to, $subject, $body)) {
         header("Location: contact.html?emailSuccess=true");
